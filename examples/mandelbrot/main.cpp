969ebd4622aacc9c892a11e5e74d799212112c2b
Richard <richard@hex-edge.com>
2022-09-17 20:24:16 +0100

    Made the mandelbrot more accurate and turned into a simple benchmark

--- examples/mandelbrot/main.cpp
+++ examples/mandelbrot/main.cpp
@@ -1,59 +1,63 @@
 #include <iostream>
 #include <cstdlib>
+#include <vector>
+#include <array>
 #include <time.h>
 #include <thread>
 #include <math.h>
+#include <chrono>
 #include <unistd.h>
 
 #include "Tiny2D.h"
 
-static uint8_t RED[256];
-static uint8_t GREEN[256];
-static uint8_t BLUE[256];
+static const int MaxItterartions = 1000;
+
+static std::array<uint8_t,MaxItterartions> RED;
+static std::array<uint8_t,MaxItterartions> GREEN;
+static std::array<uint8_t,MaxItterartions> BLUE;
+static double fyInc;
+static double fxInc;
+static double xMul = 0.3822701;
 
 bool KeepGoing = true;
 
 class Mandelbrot
 {
 public:
-	Mandelbrot():MaxItterartions(255)
+	Mandelbrot()
 	{
 	}
 
-	void Update(tiny2d::DrawBuffer& RT,int pYStart,int pYStep,float pZoom)
+	void Update(tiny2d::DrawBuffer& RT,int pYStart,int pYStep,double pZoom)
 	{
-		float fy = -1 + ((pZoom - 1.0f)*0.2f);
-		const float fyInc = 2.0f / (float)RT.GetHeight();
-		const float fxInc = 3.5f / (float)RT.GetWidth();
+		double fy = -1 + ((pZoom - 1.0)*0.2001401);
 
 		fy += (fyInc*pYStart);
 		for(int y = pYStart ; y < RT.GetHeight() && KeepGoing ;y += pYStep , fy += (fyInc*pYStep))
 		{
-			float fx = -2.5f + ((pZoom - 1.0f)*0.385f);
+			double fx = -2.5 + ((pZoom - 1.0) * xMul);
 
 			for(int x = 0 ; x < RT.GetWidth() ;x++ , fx += fxInc)
 			{
-				int i =  GetIndex(fx/pZoom,fy/pZoom);
+				const int i = GetIndex(fx/pZoom,fy/pZoom);
 				RT.WritePixel(x,y,RED[i],GREEN[i],BLUE[i]);
 			}
 		}
 	}
 
 private:
-	const int MaxItterartions;
-
-	int GetIndex(float x0,float y0)
+	int GetIndex(double x0,double y0)
 	{
 		//x0 = scaled x coordinate of pixel (scaled to lie in the Mandelbrot X scale (-2.5, 1))
 		//y0 = scaled y coordinate of pixel (scaled to lie in the Mandelbrot Y scale (-1, 1))
 
-		float x = 0;
-		float y = 0;
+		double x = 0;
+		double y = 0;
 		int iteration = 0;
-		while( x*x + y*y < 4 && iteration < MaxItterartions )
+		while( x*x + y*y < 2 && iteration < MaxItterartions )
 		{
-			float tx = x*x - y*y + x0;
-			float ty = 2.0f*x*y + y0;
+			const double tx = x*x - y*y + x0;
+			const double ty = 2.0*x*y + y0;
 
 			x = tx;
 			y = ty;
@@ -65,33 +69,24 @@ private:
 
 };
 
-void Render(float pZoom,tiny2d::DrawBuffer& RT)
+void Render(double pZoom,tiny2d::DrawBuffer& RT)
 {
-	std::thread thread1 = std::thread([pZoom,&RT](){
-		Mandelbrot Cool;
-		Cool.Update(RT,1,4,pZoom);
-	});
+	const int numThreads = std::thread::hardware_concurrency();
+	std::vector<std::thread> threads;
 
-	std::thread thread2 = std::thread([pZoom,&RT](){
-		Mandelbrot Cool;
-		Cool.Update(RT,2,4,pZoom);
-	});
-
-	std::thread thread3 = std::thread([pZoom,&RT](){
-		Mandelbrot Cool;
-		Cool.Update(RT,3,4,pZoom);
-	});
-
-	std::thread thread4 = std::thread([pZoom,&RT](){
-		Mandelbrot Cool;
-		Cool.Update(RT,4,4,pZoom);
-	});
+	for(int y = 0 ; y < numThreads ; y++ )
+	{
+		threads.emplace_back([pZoom,&RT,y,numThreads](){
+			Mandelbrot Cool;
+			Cool.Update(RT,y,numThreads,pZoom);
+		});
+	}
 
 	// Wait till all done.
-	thread1.join();
-	thread2.join();
-	thread3.join();
-	thread4.join();
+	for( auto &t : threads )
+	{
+		t.join();
+	}
 }
 
 int main(int argc, char *argv[])
@@ -103,27 +98,49 @@ int main(int argc, char *argv[])
 	srand(time(NULL));
 
     tiny2d::DrawBuffer RT(FB);
+
+	fyInc = 2.0 / (double)RT.GetHeight();
+	fxInc = 3.5 / (double)RT.GetWidth();
+
 	RT.Clear(0,0,0);
 
 	// Build colours.
-	for( int i = 0 ; i < 256 ; i++ )
+	for( int i = 0 ; i < MaxItterartions ; i++ )
 	{
-		tiny2d::HSV2RGB( (float)i * (360.0f / 255.0f) ,1.0f,1.0f - (float)i / 600.0f,RED[i],GREEN[i],BLUE[i]);		
+		tiny2d::HSV2RGB( (double)i * (360.0f / ((double)MaxItterartions)) ,1.0f,1.0f - (double)i / 600.0f,RED[i],GREEN[i],BLUE[i]);		
 	}
+	RED[MaxItterartions-1] = 0;
+	GREEN[MaxItterartions-1] = 0;
+	BLUE[MaxItterartions-1] = 0;
 
-	float zoom,zoomstep;
+	double zoom,zoomstep;
 
+	zoom = 0.1f;
 	zoomstep = 1.0f;
-	zoom = 1.0f;
-	while( (KeepGoing = FB->GetKeepGoing()) == true )
+
+	// Timer thread. Sets KeepGoing to false after a set time.
+	// Score is number of loops in one minute.
+	std::thread timer([]
+	{
+		using namespace std::chrono_literals;
+		std::this_thread::sleep_for(5min);
+		KeepGoing = false;
+	});
+
+	int loops = 0;
+	while( KeepGoing && FB->GetKeepGoing() )
 	{
 		Render(zoom,RT);
 		FB->Present(RT);
 		// Now zoom in a bit.
 		zoom += zoomstep;
-		if( zoomstep < 10.0f )
-			zoomstep += 0.5f;
+		zoomstep *= 1.001f;
+		loops++;
 	};
+	KeepGoing = false;
+	timer.join();
+
+	std::clog << "Benchmark score == " << loops << "\n";
 
 	delete FB;
 	return 0;
